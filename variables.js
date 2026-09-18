e3191497337c819c0004e4413527083aeb3b1c79
wobay2003-alt <wobay2003@gmail.com>
2026-09-18 12:19:22 -0600

    Initial commit

---
+++ variables.js
@@ -0,0 +1,32 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+area = radius * radius * PI;
+
+console.log(area);
+
+// type coersion
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+
+// Scope
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
